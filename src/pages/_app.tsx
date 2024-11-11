16c6b32414c0d487657bd00b9d196c544559e81b
henriqueguareschi <henriqueguareschi@outlook.com>
2024-11-11 16:57:59 -0300

    feat: app header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,23 @@
 import type { AppProps } from "next/app";
-import { globalStyles } from "./styles/global";
+import { globalStyles } from "../styles/global";
+import Image from "next/image";
+import logoImg from "../assets/logo.svg";
+import { Container, Header } from "../styles/pages/app";
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <Image
+          src={logoImg.src}
+          width={logoImg.width}
+          height={logoImg.height}
+          alt="Logo"
+        />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
