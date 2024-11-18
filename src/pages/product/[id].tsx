2c9f7d3267332a628be7885377bea6beecc2db4d
henriqueguareschi <henriqueguareschi@outlook.com>
2024-11-17 21:09:16 -0300

    feat: product page styles

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,27 @@
-import { useRouter } from "next/router";
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "@/styles/pages/product";
 
 export default function Product() {
-  const { query } = useRouter();
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
 
-  return <h1>Product: {JSON.stringify(query)}</h1>;
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis
+          at dolores explicabo cumque quisquam placeat quis alias dolorum esse,
+          fugit nemo adipisci aspernatur eaque a repellat, repudiandae illo
+          officia! Quidem.
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
